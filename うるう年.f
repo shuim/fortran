dd29a9f95fc6c89627dcd1c2305e20dbf385da76
shuim <shunnsuke.imai@gmail.com>
2019-03-10 22:03:24 +0900

    MESSAGE ADDED

--- うるう年.f
+++ うるう年.f
@@ -3,7 +3,7 @@ C  入力した整数値の年がうるう年か判定するプログラム
 C**************************************************
       INTEGER INPUT
       LOGICAL ISURUU
-
+      WRITE(*,*)"判定したい年の整数値を入力してください"
       READ(*,*)INPUT
       IF(ISURUU(INPUT)) THEN
         WRITE(*,*)"入力した整数はうるう年です。"
